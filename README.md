9f2e2031524f5b3c30baf34da8e0406d06d85353
Chuan Ji <jichu4n@gmail.com>
2016-04-27 01:25:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# fish-command-timer
-Fish shell extension for printing execution time for each command.
+fish-command-timer
+==================
+
+fish shell extension for printing timing information for each command line
+executed.
+
+Usage
+-----
+
+After the execution of each command line, the script prints out the total
+execution time (up to millisecond precision), followed by the current time. The
+execution time is formatted to be human readable; e.g., `2h 7m 42s301`.
+
+Demo:
+
+![Demo Screen-cap](https://github.com/jichuan89/bash-command-timer/raw/master/bash_command_timer_screenshot.gif)
+
+Requirements
+------------
+
+This script requires fish shell 2.2 or above. It should run pretty much out of
+the box on modern Linux and Mac OS X systems. Please report any
+incompatibilities on
+[on GitHub](https://github.com/jichu4n/fish-command-timer/issues).
+
+Installation
+------------
+
+To set up this extension, you can
+
+1. Download `fish_command_timer.fish` somewhere, and add the following to your
+   `~/.config/fish/config.fish` (replace with actual path where you saved the script):
+
+   ```bash
+   source ~/.fish_command_timer.fish
+   ```
+
+2. Alternatively, you can simply copy and paste the contents of
+   `fish_command_timer.fish` into your `~/.config/fish/config.fish`.
+
+That's it :)
+
+Settings
+--------
+
+You can use the following options to tweak the behavior of the script. You can
+either make the changes in-place (at the top of the script) or put them after
+sourcing the script in your `.bashrc`. You can also modify them on-the-fly if
+you want the changes to only affect your current Bash session.
+
+* `set fish_command_timer_enabled`: Unsetting this variable with `set -e` disables
+  this script.
+* `set fish_command_timer_color blue`: The color of the output. This should be a
+  color string recognized by fish's set_color command, as described
+  (http://fishshell.com/docs/current/commands.html#set_color)[here]. If not set,
+  the output will be in the default color.
+* `set fish_command_timer_time_format '%b %d %I:%M%p'`: The display format of
+  the current time.  This is a strftime format string (see
+  http://strftime.org/). If empty, the current time will not be printed.
+* `set fish_command_timer_millis`: Whether to print timings to millisecond
+  precision. If unset, will print timings up to seconds.
+
---
+++ fish_command_timer.fish
@@ -0,0 +1,192 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#    Copyright (C) 2016 Chuan Ji <jichuan89@gmail.com>                        #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#     http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# A fish shell script for printing execution time for each command.
+#
+# For the most up-to-date version, as well as further information and
+# installation instructions, please visit the GitHub project page at
+#     https://github.com/jichuan89/fish-command-timer
+#
+# Requires fish 2.2 or above.
+
+# SETTINGS
+# ========
+#
+# Whether to enable the command timer by default.
+#
+# To temporarily disable the printing of timing information, type the following
+# in a session:
+#     set -e fish_command_timer_enabled
+# To re-enable:
+#     set fish_command_timer_enabled
+set fish_command_timer_enabled
+
+# The color of the output.
+#
+# This should be a color string accepted by fish's set_color command, as
+# described here:
+#
+#     http://fishshell.com/docs/current/commands.html#set_color
+#
+# If empty, disable colored output. Set it to empty if your terminal does not
+# support colors.
+set fish_command_timer_color blue
+
+# The display format of the current time.
+#
+# This is a strftime format string (see http://strftime.org/). To tweak the
+# display format of the current time, change the following line to your desired
+# pattern.
+#
+# If empty, disables printing of current time.
+set fish_command_timer_time_format '%b %d %I:%M%p'
+
+# Whether to print command timings up to millisecond precision.
+#
+# If unset, will print up to seconds precision.
+set fish_command_timer_millis
+
+
+# IMPLEMENTATION
+# ==============
+
+# fish_command_timer_get_ts:
+#
+# Command to print out the current time in nanoseconds. This is required
+# because the "date" command in OS X and BSD do not support the %N sequence.
+#
+# fish_command_timer_print_time:
+#
+# Command to print out a timestamp using fish_command_timer_time_format. The
+# timestamp should be in seconds. This is required because the "date" command in
+# Linux and OS X use different arguments to specify the timestamp to print.
+if date +'%N' | grep -qv 'N'
+  function fish_command_timer_get_ts
+    date '+%s%N'
+  end
+  function fish_command_timer_print_time
+    date --date="@$argv[1]" +"$fish_command_timer_time_format"
+  end
+else if type gdate > /dev/null ^ /dev/null; and gdate +'%N' | grep -qv 'N'
+  function fish_command_timer_get_ts
+    gdate '+%s%N'
+  end
+  function fish_command_timer_print_time
+    gdate --date="@$argv[1]" +"$fish_command_timer_time_format"
+  end
+else if type perl > /dev/null ^ /dev/null; then
+  function fish_command_timer_get_ts
+    perl -MTime::HiRes -e 'printf(\"%d\",Time::HiRes::time()*1000000000)'
+  end
+  function fish_command_timer_print_time
+    date -r "$argv[1]" +"$fish_command_timer_time_format"
+  end
+else
+  echo 'No compatible date, gdate or perl commands found, not enabling fish command timer'
+  set -e fish_command_timer_enabled
+end
+
+# fish_command_timer_strlen:
+#
+# Command to print out the length of a string. This is required because the expr
+# command behaves differently on Linux and OS X. On fish 2.3+, we will use the
+# "string" builtin.
+if type string > /dev/null ^ /dev/null
+  function fish_command_timer_strlen
+    string length "$argv[1]"
+  end
+else if expr length + "1" > /dev/null ^ /dev/null
+  function fish_command_timer_strlen
+    expr length + "$argv[1]"
+  end
+else if type wc > /dev/null ^ /dev/null; and type tr > /dev/null ^ /dev/null
+  function fish_command_timer_strlen
+    echo -n "$argv[1]" | wc -c | tr -d ' '
+  end
+else
+  echo 'No compatible string, expr, or wc commands found, not enabling fish command timer'
+  set -e fish_command_timer_enabled
+end
+
+# The fish_preexec event is fired before executing a command line.
+function -e fish_preexec fish_command_timer_preexec
+  if not set -q fish_command_timer_enabled
+    return
+  end
+  set fish_command_timer_start_time (fish_command_timer_get_time)
+end
+
+# The fish_postexec event is fired after executing a command line.
+function -e fish_postexec fish_command_timer_postexec
+  if not set -q fish_command_timer_enabled
+    return
+  end
+
+  set -l MSEC 1000000
+  set -l SEC (math "1000 * $MSEC")
+  set -l MIN (math "60 * $SEC")
+  set -l HOUR (math "60 * $MIN")
+  set -l DAY (math "24 * $HOUR")
+
+  set -l command_start_time $fish_command_timer_start_time
+  set -l command_end_time (fish_command_timer_get_time)
+  set -l command_time (math "$command_end_time - $command_start_time")
+  set -l num_days (math "$command_time / $DAY")
+  set -l num_hours (math "$command_time % $DAY / $HOUR")
+  set -l num_mins (math "$command_time % $HOUR / $MIN")
+  set -l num_secs (math "$command_time % $MIN / $SEC")
+  set -l num_msecs (math "$command_time % $SEC / $MSEC")
+  set -l time_str ""
+  if [ $num_days -gt 0 ]
+    set time_str {$time_str}{$num_days}"d "
+  end
+  if [ $num_hours -gt 0 ]
+    set time_str {$time_str}{$num_hours}"h "
+  end
+  if [ $num_mins -gt 0 ]
+    set time_str {$time_str}{$num_mins}"m "
+  end
+  set -l num_msecs_pretty ''
+  if set -q fish_command_timer_millis
+    set num_msecs_pretty (printf '%03d' $num_msecs)
+  end
+  set time_str {$time_str}{$num_secs}s{$num_msecs_pretty}
+
+  set -l now_str (fish_command_timer_print_time (math "$command_end_time / $SEC"))
+  set -l output_str
+  if [ -n "$now_str" ]
+    set output_str "[ $time_str | $now_str ]"
+  else
+    set output_str "[ $time_str ]"
+  end
+  set -l output_str_colored
+  if set -q fish_command_timer_color
+    set output_str_colored (set_color $fish_command_timer_color)"$output_str"(set_color normal)
+  else
+    set output_str_colored "$output_str"
+  end
+  set -l output_str_length (fish_command_timer_strlen "$output_str")
+
+  # Move to the end of the line. This will NOT wrap to the next line.
+  echo -ne "\033["{$COLUMNS}"C"
+  # Move back (length of output_str) columns.
+  echo -ne "\033["{$output_str_length}"D"
+  # Finally, print output.
+  echo -e "$output_str_colored"
+end
+
